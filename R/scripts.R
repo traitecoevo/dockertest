5db0195245e316953f1d12d90dc7d476b1e6143b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-09-08 10:59:20 +0100

    Expose no-cache and no-prepare options to command line interface

--- R/scripts.R
+++ R/scripts.R
@@ -4,7 +4,10 @@ main <- function(args=commandArgs(TRUE)) {
     opts$type <- "test"
   }
   if (isTRUE(opts$build)) {
-    build(opts$type, machine=opts$machine)
+    build(opts$type,
+          prepare=!opts$"no-prepare",
+          use_cache=!opts$"no-cache",
+          machine=opts$machine)
   } else if (isTRUE(opts$prepare)) {
     prepare(project_info(opts$type))
   } else if (isTRUE(opts$launch)) {
@@ -23,11 +26,13 @@ main <- function(args=commandArgs(TRUE)) {
 parse_main_args <- function(args) {
   'Usage:
   dockertest prepare [<type>]
-  dockertest build  [--machine=NAME] [<type>]
+  dockertest build  [--machine=NAME] [--no-prepare] [--no-cache] [<type>]
   dockertest launch [--machine=NAME] [<type>] [--dry-run] [<args> ...]
 
   Options:
   --machine=NAME  docker machine name to use (non Linux) [default: default]
+  --no-prepare    don\'t reclone/recreate Dockerfile
+  --no-cache      skip docker\'s cache on building
   --dry-run       don\'t launch container but print command to do so' -> str
   docopt_parse(str, args)
 }
